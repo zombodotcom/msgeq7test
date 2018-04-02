f2fb7e1643595431913365bd3091f490f0de95ca
zombodotcom <tommysciano@gmail.com>
2018-04-02 17:26:02 -0600

    msgeq7 test arduino baseline

---
+++ msgeq7test.ino
@@ -0,0 +1,155 @@
+// Pin definitions
+#define MSGEQ7_AUDIO_PIN A0
+#define MSGEQ7_STROBE_PIN 2
+#define MSGEQ7_RESET_PIN  3
+
+#define AUDIODELAY 0
+
+// Smooth/average settings
+#define SPECTRUMSMOOTH 0.08
+#define PEAKDECAY 0.01
+#define NOISEFLOOR 65
+
+// AGC settings
+#define AGCSMOOTH 0.004
+#define GAINUPPERLIMIT 15.0
+#define GAINLOWERLIMIT 0.1
+
+// Global variables
+unsigned int spectrumValue[7];  // holds raw adc values
+float spectrumDecay[7] = {0};   // holds time-averaged values
+float spectrumPeaks[7] = {0};   // holds peak values
+float audioAvg = 270.0;
+float gainAGC = 0.0;
+
+uint8_t spectrumByte[7];        // holds 8-bit adjusted adc values
+
+uint8_t spectrumAvg;
+
+unsigned long currentMillis; // store current loop's millis value
+unsigned long audioMillis; // store time of last audio update
+
+void initializeAudio() {
+  pinMode(MSGEQ7_AUDIO_PIN, INPUT);
+  pinMode(MSGEQ7_RESET_PIN, OUTPUT);
+  pinMode(MSGEQ7_STROBE_PIN, OUTPUT);
+
+  digitalWrite(MSGEQ7_RESET_PIN, LOW);
+  digitalWrite(MSGEQ7_STROBE_PIN, HIGH);
+}
+
+void readAudio() {
+  static PROGMEM const byte spectrumFactors[7] = {9, 11, 13, 13, 12, 12, 13};
+
+  // reset MSGEQ7 to first frequency bin
+  digitalWrite(MSGEQ7_RESET_PIN, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(MSGEQ7_RESET_PIN, LOW);
+
+  // store sum of values for AGC
+  int analogsum = 0;
+
+  // cycle through each MSGEQ7 bin and read the analog values
+  for (int i = 0; i < 7; i++) {
+
+    // set up the MSGEQ7
+    digitalWrite(MSGEQ7_STROBE_PIN, LOW);
+    delayMicroseconds(50); // to allow the output to settle
+
+    // read the analog value
+    spectrumValue[i] = analogRead(MSGEQ7_AUDIO_PIN);
+    digitalWrite(MSGEQ7_STROBE_PIN, HIGH);
+
+    // noise floor filter
+    if (spectrumValue[i] < NOISEFLOOR) {
+      spectrumValue[i] = 0;
+    } else {
+      spectrumValue[i] -= NOISEFLOOR;
+    }
+
+    // apply correction factor per frequency bin
+    spectrumValue[i] = (spectrumValue[i] * pgm_read_byte_near(spectrumFactors + i)) / 10;
+
+    // prepare average for AGC
+    analogsum += spectrumValue[i];
+
+    // apply current gain value
+    spectrumValue[i] *= gainAGC;
+
+    // process time-averaged values
+    spectrumDecay[i] = (1.0 - SPECTRUMSMOOTH) * spectrumDecay[i] + SPECTRUMSMOOTH * spectrumValue[i];
+
+    // process peak values
+    if (spectrumPeaks[i] < spectrumDecay[i]) spectrumPeaks[i] = spectrumDecay[i];
+    spectrumPeaks[i] = spectrumPeaks[i] * (1.0 - PEAKDECAY);
+
+    spectrumByte[i] = spectrumValue[i] / 4;
+  }
+
+  // Calculate audio levels for automatic gain
+  audioAvg = (1.0 - AGCSMOOTH) * audioAvg + AGCSMOOTH * (analogsum / 7.0);
+
+  spectrumAvg = (analogsum / 7.0) / 4;
+
+  // Calculate gain adjustment factor
+  gainAGC = 270.0 / audioAvg;
+  if (gainAGC > GAINUPPERLIMIT) gainAGC = GAINUPPERLIMIT;
+  if (gainAGC < GAINLOWERLIMIT) gainAGC = GAINLOWERLIMIT;
+}
+
+// Attempt at beat detection
+byte beatTriggered = 0;
+#define beatLevel 20.0
+#define beatDeadzone 30.0
+#define beatDelay 50
+float lastBeatVal = 0;
+byte beatDetect() {
+  static float beatAvg = 0;
+  static unsigned long lastBeatMillis;
+  float specCombo = (spectrumDecay[0] + spectrumDecay[1]) / 2.0;
+  beatAvg = (1.0 - AGCSMOOTH) * beatAvg + AGCSMOOTH * specCombo;
+
+  if (lastBeatVal < beatAvg) lastBeatVal = beatAvg;
+  if ((specCombo - beatAvg) > beatLevel && beatTriggered == 0 && currentMillis - lastBeatMillis > beatDelay) {
+    beatTriggered = 1;
+    lastBeatVal = specCombo;
+    lastBeatMillis = currentMillis;
+    return 1;
+  } else if ((lastBeatVal - specCombo) > beatDeadzone) {
+    beatTriggered = 0;
+    return 0;
+  } else {
+    return 0;
+  }
+}
+
+void print_audio() {
+  for (int band = 0; band < 7; band++) {
+    Serial.print(spectrumByte[band]);
+    Serial.print("\t");
+  }
+  Serial.println();
+}
+
+
+void setup() {
+  Serial.begin(19200);
+  delay(100);
+  initializeAudio();
+  
+
+}
+
+
+void loop() {
+  currentMillis = millis(); // save the current timer value
+  print_audio();
+  // analyze the audio input
+//  if (currentMillis - audioMillis > AUDIODELAY) {
+//    audioMillis = currentMillis;
+//    readAudio();
+//  }
+readAudio();
+}
+
+  
